0ba9192505df0ae8ac61c37d4963b22213736bc7
elliotgonzalez123 <elliotgrey@gmail.com>
2022-10-21 14:04:48 -0400

    text change

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+        <h2>CI/CD with GH Actions Complete!</h2>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
