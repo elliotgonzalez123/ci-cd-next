e33cdfdb5d864e1d99753814a7f12bfadb04087c
Elliot Gonzalez <49325153+elliotgonzalez123@users.noreply.github.com>
2022-10-21 15:48:05 -0400

    add h3

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
         <h2>CI/CD with GH Actions Complete! With tests too.</h2>
+        <h3>Preview builds enabled</h3>
         <p className={styles.description}>
           Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
